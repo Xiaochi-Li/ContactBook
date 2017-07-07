8ee4e9526506740261076d729dcf599495e4a74d
Unknown <294188719@qq.com>
2017-07-07 20:41:34 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class ContactList extends React.Component{
-  render(){
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map((person,index) =>
-        (<li key={index}> {person.name}</li>))}
-    </ol>
-  }
-}
-
+//
+// class ContactList extends React.Component{
+//   render(){
+//     const people = this.props.contacts
+//
+//     return <ol>
+//       {people.map((person,index) =>
+//         (<li key={index}> {person.name}</li>))}
+//     </ol>
+//   }
+// }
+//
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-            {name:'Xiaochi Li'},
-            {name:'Robert Gardiner'},
-            {name:'Jie Li'}
-          ]}/>
+      <div >
+        Hello World!
       </div>
     );
   }
