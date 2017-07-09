adca5a3a8335c9fdb21d866acba27eedeec0253d
Unknown <294188719@qq.com>
2017-07-09 12:04:05 +0800

    Test proptype

--- src/App.js
+++ src/App.js
@@ -3,29 +3,42 @@ import ListContacts from './ListContacts'
 
 class App extends Component {
   state = {
-    contacts : [
-    {
-      "id": "ryan",
-      "name": "Ryan Florence",
-      "email": "ryan@reacttraining.com",
-      "avatarURL": "http://localhost:5001/ryan.jpg"
-    },
-    {
-      "id": "michael",
-      "name": "Michael Jackson",
-      "email": "michael@reacttraining.com",
-      "avatarURL": "http://localhost:5001/michael.jpg"
-    },
-    {
-      "id": "tyler",
-      "name": "Tyler McGinnis",
-      "email": "tyler@reacttraining.com",
-      "avatarURL": "http://localhost:5001/tyler.jpg"
-    }
-  ]}
+    contacts :
+      [
+        {
+          "id": "ryan",
+          "name": "Ryan Florence",
+          "email": "ryan@reacttraining.com",
+          "avatarURL": "http://localhost:5001/ryan.jpg"
+        },
+        {
+          "id": "michael",
+          "name": "Michael Jackson",
+          "email": "michael@reacttraining.com",
+          "avatarURL": "http://localhost:5001/michael.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "email": "tyler@reacttraining.com",
+          "avatarURL": "http://localhost:5001/tyler.jpg"
+        }
+    ]
+  }
+
+  removeContact = (contact) => {
+    this.setState((previouState) =>({
+      contacts:this.state.contacts.filter(
+        (eachContact) => eachContact.id != contact.id
+      )
+    })
+  )
+  }
   render() {
     return (
-      <ListContacts contacts={this.state.contacts}/>
+      <ListContacts
+        onDeleteContact={this.removeContact}
+        contacts={{}}/>
     );
   }
 }
