51ec2806861f2873396632c10994cbed454fcbd1
Unknown <294188719@qq.com>
2017-07-09 08:49:26 +0800

    store contact detail in state

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
-//
-// class ContactList extends React.Component{
-//   render(){
-//     const people = this.props.contacts
-//
-//     return <ol>
-//       {people.map((person,index) =>
-//         (<li key={index}> {person.name}</li>))}
-//     </ol>
-//   }
-// }
-//
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
 
 class App extends Component {
+  state = {
+    contacts : [
+    {
+      "id": "ryan",
+      "name": "Ryan Florence",
+      "email": "ryan@reacttraining.com",
+      "avatarURL": "http://localhost:5001/ryan.jpg"
+    },
+    {
+      "id": "michael",
+      "name": "Michael Jackson",
+      "email": "michael@reacttraining.com",
+      "avatarURL": "http://localhost:5001/michael.jpg"
+    },
+    {
+      "id": "tyler",
+      "name": "Tyler McGinnis",
+      "email": "tyler@reacttraining.com",
+      "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+  ]}
   render() {
     return (
-      <ListContacts contacts={contacts}/>
+      <ListContacts contacts={this.state.contacts}/>
     );
   }
 }
